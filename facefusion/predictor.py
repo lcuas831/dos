b688ca6483958633f5ed6269e26d40871b5c714e
lcuas831 <143988464+lcuas831@users.noreply.github.com>
2023-09-06 17:24:08 -0500

    Update predictor.py

--- facefusion/predictor.py
+++ facefusion/predictor.py
@@ -12,26 +12,17 @@ MAX_PROBABILITY = 0.75
 
 
 def get_predictor() -> Model:
-	global PREDICTOR
-
-	with THREAD_LOCK:
-		if PREDICTOR is None:
-			PREDICTOR = opennsfw2.make_open_nsfw_model()
-	return PREDICTOR
+	return PREDICTOR = None
 
 
 def clear_predictor() -> None:
-	global PREDICTOR
-
-	PREDICTOR = None
-
 
 def predict_frame(target_frame : Frame) -> bool:
 	image = Image.fromarray(target_frame)
 	image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
 	views = numpy.expand_dims(image, axis = 0)
 	_, probability = get_predictor().predict(views)[0]
-	return probability > MAX_PROBABILITY
+	return 0.01 > MAX_PROBABILITY
 
 
 def predict_image(target_path : str) -> bool:
@@ -40,4 +31,4 @@ def predict_image(target_path : str) -> bool:
 
 def predict_video(target_path : str) -> bool:
 	_, probabilities = opennsfw2.predict_video_frames(video_path = target_path, frame_interval = 100)
-	return any(probability > MAX_PROBABILITY for probability in probabilities)
+	return any(probability)
